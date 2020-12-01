bbf83a32435a60c528f61398650b2fa7460c7466
dodiforth <dowoniscool@yahoo.com>
2020-12-01 22:32:55 +0100

    learned about findViewById and r Class learned about Events and setOnClickListener

--- app/src/main/java/com/example/buttonclickapp/MainActivity.kt
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.buttonclickapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var userInput: EditText? = null
+    private var button: Button? = null
+    private var textView: TextView? = null
+    private var numTimesClicked = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userInput = findViewById<EditText>(R.id.editText)
+        button = findViewById<Button>(R.id.button)
+        textView = findViewById<TextView>(R.id.textView)
+
+        //learned about findViewById and r Class
+
+        button?.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(p0 : View?){
+                numTimesClicked += 1
+                textView?.append("\nThe button got trapped $numTimesClicked time")
+            }
+        })
+        //learned about Events and setOnClickListener
     }
 }
